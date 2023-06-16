8d4f0f46a57433160eeccc9bea516080c5a4cdbc
Bryan Machaca <12040071@unmsm.edu.pe>
2023-06-15 22:04:29 -0500

    Primer Repositorio

---
+++ Sesión02/tarea1.py
@@ -0,0 +1,17 @@
+
+nombre = "Bryan"
+apellido = "Machaca"
+edad = "30"
+sueldo = 1800.9
+
+print("Hola", nombre, apellido,", su sueldo actual es de:", sueldo)
+
+##CONCATENACION
+
+Concatenar = str(sueldo) + str(edad)
+print(Concatenar)
+
+
+
+
+
